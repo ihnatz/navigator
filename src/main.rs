fac7a4750847accb7f750addd07476e8f39eeade
Ignat Zakrevsky <iezakrevsky@gmail.com>
2024-09-17 22:52:24 +0100

    add writing to file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use std::fs::File;
+use std::io::Write;
+
 mod config;
 mod ui;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let menu = config::Menu::read_config();
-    if let Some(command) = ui::main(&menu.unwrap()) {
-        println!("{command}");
+    let output_path = "/tmp/navigator_output.txt";
+
+    let mut file = File::create(output_path)?;
+
+    if let Some(cmd) = ui::main(&menu.unwrap()) {
+        file.write_all(cmd.as_bytes())?;
+    } else {
+        file.write_all(b"")?;
     }
+
+    Ok(())
 }
